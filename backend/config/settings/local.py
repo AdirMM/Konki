fc2dfb86f9135fdc77e748f70c422e8ce82d64be
IvsKevin <156042915+IvsKevin@users.noreply.github.com>
2025-05-01 20:54:49 -0700

    Backend init

---
+++ backend/config/settings/local.py
@@ -0,0 +1,22 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
